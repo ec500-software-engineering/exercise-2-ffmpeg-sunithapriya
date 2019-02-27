ac233b2a35337f57a97530925381b7c2a094c878
Sunitha Priyadarshini Sampath <sunithap@bu.edu>
2019-02-27 14:57:19 -0500

    Added check duration function

--- test_processVideo.py
+++ test_processVideo.py
@@ -2,7 +2,8 @@ import pytest
 from ffmpeg import processVideo
 from pathlib import Path
 import subprocess
-import myffmpeg
+from pytest import approx
+import json
 
 @pytest.fixture
 def genpat(tmp_path) -> Path:
@@ -14,7 +15,21 @@ def genpat(tmp_path) -> Path:
 	subprocess.check_call(['ffmpeg', '-v', 'warning', '-f', 'lavfi', '-i', 'smptebars', '-t', '5.', str(vidfn)])
 	return vidfn
 
+def duration(path) -> Path:
+	return float(json.loads(subprocess.check_output(['ffprobe','-print_format','json','-show_format',path]))['format']['duration'])
+
 def test_processVideo(genpat):
 
 	fn = genpat
 	result = processVideo(fn)
+
+	orig_duration = duration(fn)
+	duration_720 = duration(result[0])
+	duration_480 = duration(result[1])
+
+	assert orig_duration == approx(duration_720)
+	assert orig_duration == approx(duration_480)
+
+
+
+
