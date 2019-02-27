232f5a3f37b3703802bcc129a47f7f7beaa0751b
Sunitha Priyadarshini Sampath <sunithap@bu.edu>
2019-02-27 18:04:56 -0500

    error fix

--- test_processVideo.py
+++ test_processVideo.py
@@ -15,33 +15,17 @@ import json
 # 	subprocess.check_call(['ffmpeg', '-v', 'warning', '-f', 'lavfi', '-i', 'smptebars', '-t', '5.', vidfn])
 # 	return vidfn
 
-def get_duration(file):
-    """Get the duration of a video using ffprobe."""
-    cmd = 'ffprobe -i {} -show_entries format=duration -v quiet -of csv="p=0"'.format(file)
-    output = subprocess.check_output(
-        cmd,
-        shell=True, # Let this run in the shell
-        stderr=subprocess.STDOUT
-    )
-    # return round(float(output))  # ugly, but rounds your seconds up or down
-    return float(output)
-
-# def getLength(input_video):
-# 	result = subprocess.Popen('ffprobe -i input_video -show_entries format=duration -v quiet -of csv="p=0"', stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
-# 	output = result.communicate()
-# 	return output[0]
-
-# def duration(path):
-# 	return float(json.loads(subprocess.check_output("ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 video2.mp4")))
+def duration(path):
+	return float(json.loads(subprocess.check_output(['ffprobe','-print_format','json','-show_format',path]))['format']['duration'])
 
 def test_processVideo():
 
 	fn = 'video2.mp4'
 	result = processVideo(fn)
 
-	orig_duration = get_duration(fn)
-	duration_720 = get_duration(result[0])
-	duration_480 = get_duration(result[1])
+	orig_duration = duration(fn)
+	duration_720 = duration(result[0])
+	duration_480 = duration(result[1])
 
 	assert orig_duration == duration_720
 	assert orig_duration == duration_480
